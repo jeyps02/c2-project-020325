c68074679fd4e48bb7a4cd3eb2f276826d8ec813
jeyps02 <jpaulpascual19@gmail.com>
2025-03-30 22:27:53 +0800

    App.js

--- src/App.js
+++ src/App.js
@@ -4,17 +4,18 @@ import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
-import Invoices from "./scenes/invoices";
-import Contacts from "./scenes/contacts";
+import Invoices from "./scenes/audit logs";
+import Contacts from "./scenes/policies";
 import Bar from "./scenes/bar";
 import Form from "./scenes/form";
 import Line from "./scenes/line";
 import Pie from "./scenes/pie";
 import FAQ from "./scenes/faq";
 import Geography from "./scenes/geography";
+import Calendar from "./scenes/calendar/calendar";
+import LiveFeed from "./scenes/Live Feed"; // Import the Live Feed page
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
-import Calendar from "./scenes/calendar/calendar";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -40,6 +41,7 @@ function App() {
               <Route path="/faq" element={<FAQ />} />
               <Route path="/calendar" element={<Calendar />} />
               <Route path="/geography" element={<Geography />} />
+              <Route path="/live-feed" element={<LiveFeed />} /> {/* Add the Live Feed route */}
             </Routes>
           </main>
         </div>
